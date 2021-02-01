fa23c47adaab6662b09168b9aba9fe3b59527317
Shao-Hung Hsiao <shaohungh@gmail.com>
2021-01-31 18:11:51 -0800

    support flv + remove keyboard bindings

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-$(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
-    $('#play-btn').on('click', function () {
-        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
-    });
-});
+window.onload = () => {
+    const srcInput = document.querySelector('#m3u8-placeholder');
+    srcInput.value = localStorage.getItem('m3u8-link') || '';
+    document.querySelector('#play-btn').onclick = () => {
+        localStorage.setItem('m3u8-link', srcInput.value);
+        window.location.href = './player' + '#' + srcInput.value;
+    }
+};
--- player/player.js
+++ player/player.js
@@ -1,64 +1,49 @@
-var video = document.getElementById('video');
+const video = document.getElementById('video');
 
-function playM3u8(url){
-  if(Hls.isSupported()) {
-      video.volume = 0.3;
-      var hls = new Hls();
-      var m3u8Url = decodeURIComponent(url)
-      hls.loadSource(m3u8Url);
-      hls.attachMedia(video);
-      hls.on(Hls.Events.MANIFEST_PARSED,function() {
-        video.play();
-      });
-      document.title = url
-    }
-	else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-		video.src = url;
-		video.addEventListener('canplay',function() {
-		  video.play();
-		});
-		video.volume = 0.3;
-		document.title = url;
-  	}
-}
-
-function playPause() {
-    video.paused?video.play():video.pause();
-}
-
-function volumeUp() {
-    if(video.volume <= 0.9) video.volume+=0.1;
-}
-
-function volumeDown() {
-    if(video.volume >= 0.1) video.volume-=0.1;
-}
-
-function seekRight() {
-    video.currentTime+=5;
-}
-
-function seekLeft() {
-    video.currentTime-=5;
-}
-
-function vidFullscreen() {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+const loadVideo = (url) => {
+    console.log(url);
+    const sourceType = checkSourceType(url);
+    video.volume = 0.3;
+    if (sourceType === 'hls' && Hls.isSupported()) {
+        const hls = new Hls();
+        const m3u8Url = decodeURIComponent(url)
+        hls.loadSource(m3u8Url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+        document.title = url
+    } else if (sourceType === 'flv' && flvjs.isSupported()) {
+        const flv = flvjs.createPlayer({
+            type: 'flv',
+            url: decodeURIComponent(url),
+            hasAudio: true,
+            hasVideo: true
+        })
+        flv.attachMediaElement(video);
+        flv.load();
+        flv.play();
+        document.title = url
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.addEventListener('canplay', function () {
+            video.play();
+        });
+        document.title = url;
     }
 }
 
-playM3u8(window.location.href.split("#")[1])
-$(window).on('load', function () {
-    $('#video').on('click', function(){this.paused?this.play():this.pause();});
-    Mousetrap.bind('space', playPause);
-    Mousetrap.bind('up', volumeUp);
-    Mousetrap.bind('down', volumeDown);
-    Mousetrap.bind('right', seekRight);
-    Mousetrap.bind('left', seekLeft);
-    Mousetrap.bind('f', vidFullscreen);
-});
+const checkSourceType = (url) => {
+    if (url.includes('.m3u8')) {
+        return 'hls';
+    } else if (url.includes('.flv')) {
+        return 'flv';
+    }
+    return "unknown";
+}
+
+loadVideo(window.location.href.split("#")[1])
+
+window.onload = () => {
+    video.focus();
+}
